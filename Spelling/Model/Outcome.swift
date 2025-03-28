59af59d9f5689c593a38e5c31a3bda2641d18a07
Josh Yim <jyim@lcs.on.ca>
2025-03-28 14:47:47 -0400

    Users can now enter guesses, guesses can be determined to be correct or incorrect, the users can now see whether their response are correct or not.

---
+++ Spelling/Model/Outcome.swift
@@ -0,0 +1,15 @@
+//
+//  Outcome.swift
+//  Spelling
+//
+//  Created by junxi Yim on 2025/3/28.
+//
+
+import Foundation
+// Tracks what is happenin
+// with a given guess
+enum Outcome: String {
+    case undetermined = " "
+    case correct = "✅"
+    case incorrect = "❌"
+}
--- Spelling/Views/QuizView.swift
+++ Spelling/Views/QuizView.swift
@@ -12,6 +12,11 @@ struct QuizView: View {
     // MARK: Stored properties
     @State var currentItem = itemsToSpell.randomElement()!
     
+    //what the user has guessed
+    @State var userGuess = ""
+    
+    //What aws the outcome of a user making a guess?
+    @State var currentOutcome: Outcome = .undetermined
     // MARK: Computed properties
     var body: some View {
         
@@ -19,11 +24,28 @@ struct QuizView: View {
             Image(currentItem.imageName)
                 .resizable()
                 .scaledToFit()
+            HStack{
+                TextField("Enter the name of the item", text: $userGuess)
+                Button {
+                    checkGuess()
+                }label:{
+                    Text("Submit")
+                }
+                Text(currentOutcome.rawValue)
+            }
         }
-        
     }
-}
-
-#Preview {
-    QuizView()
-}
+        func checkGuess() {
+            if userGuess == currentItem.word{
+                currentOutcome = .correct
+                print("Correct")
+            }else{
+                currentOutcome = .incorrect
+                print("Incorrect")
+            }
+        }
+    }
+    
+    #Preview {
+        QuizView()
+    }
